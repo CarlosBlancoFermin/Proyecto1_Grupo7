4c23e3f799d90ab86ce8fad1c2133405f712695b
CarlosBlanco <carlostrubia@gmail.com>
2023-10-23 22:49:22 +0200

    implementacion de login y pantalla de menu provisional

---
+++ app/src/main/java/com/example/proyecto_entrega2_grupo7/activities/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.proyecto_entrega2_grupo7.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyecto_entrega2_grupo7.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
