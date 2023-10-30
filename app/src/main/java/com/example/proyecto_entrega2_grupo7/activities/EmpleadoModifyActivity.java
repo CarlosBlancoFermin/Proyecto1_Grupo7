a3313a6b1cc45d5677e8505500c4beb40774818c
ZioR <amontanov@uoc.edu>
2023-10-30 11:12:00 +0100

    -Implementacion interfaz modificar Usuario

---
+++ app/src/main/java/com/example/proyecto_entrega2_grupo7/activities/EmpleadoModifyActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyecto_entrega2_grupo7.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto_entrega2_grupo7.R;
+
+public class EmpleadoModifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empleado_modify);
+    }
+}
