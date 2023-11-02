9f2fb608f205693b4953f0ee89054a67c7d98205
CarlosBlanco <carlostrubia@gmail.com>
2023-11-02 22:54:02 +0100

    activity_creditos.xml - añadido layout - añadida funcionalidad

--- app/src/main/java/com/example/proyecto_entrega2_grupo7/activities/MenuActivity.java
+++ app/src/main/java/com/example/proyecto_entrega2_grupo7/activities/MenuActivity.java
@@ -1,15 +1,31 @@
 package com.example.proyecto_entrega2_grupo7.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.proyecto_entrega2_grupo7.R;
+import com.example.proyecto_entrega2_grupo7.activities.listar.Creditos;
+import com.example.proyecto_entrega2_grupo7.activities.listar.ListarActivity;
 
 public class MenuActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+    public void btcreditos(View view){
+        Intent intent = new Intent(this, Creditos.class);
+        startActivity(intent);
+    }
+    public void btListarEmpleados(View view){
+        Intent intent = new Intent(this, ListarActivity.class);
+        startActivity(intent);
+    }
+    public void btNuevoempleado(View view){
+        Intent intent = new Intent(this, ListarActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/proyecto_entrega2_grupo7/activities/listar/Creditos.java
@@ -0,0 +1,50 @@
+package com.example.proyecto_entrega2_grupo7.activities.listar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.proyecto_entrega2_grupo7.R;
+import com.example.proyecto_entrega2_grupo7.activities.MenuActivity;
+
+public class Creditos extends AppCompatActivity {
+
+    String URl;
+    Uri link;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+    }
+    public void btcreditos2(View view){
+        URl = "https://es.linkedin.com/in/carlos-blanco-ferm%C3%ADn-b9ab13229";
+        link = Uri.parse(URl);
+        Intent intent = new Intent(Intent.ACTION_VIEW,link);
+        startActivity(intent);
+    }
+    public void btcreditos1(View view){
+        URl = "https://www.linkedin.com/in/andresmonvi/";
+        link = Uri.parse(URl);
+        Intent intent = new Intent(Intent.ACTION_VIEW,link);
+        startActivity(intent);
+    }
+    public void btcreditos3(View view){
+        URl = "https://www.linkedin.com/in/andresmonvi/";
+        link = Uri.parse(URl);
+        Intent intent = new Intent(Intent.ACTION_VIEW,link);
+        startActivity(intent);
+    }
+    public void btcodigo(View view){
+        URl = "https://github.com/CarlosBlancoFermin/Proyecto1_Grupo7";
+        link = Uri.parse(URl);
+        Intent intent = new Intent(Intent.ACTION_VIEW,link);
+        startActivity(intent);
+    }
+    public void btcreditosvolver(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+}
